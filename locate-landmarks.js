c55270d49d89aa365ba7348996bdfd2da035ecaf
Matt Rohr <67395122+mattrohr@users.noreply.github.com>
2021-05-08 18:07:16 -0500

    add head deviation calculation

--- locate-landmarks.js
+++ locate-landmarks.js
@@ -10,6 +10,9 @@ function startVideo() {
     navigator.mediaDevices.getUserMedia({ video: {} }).then((stream) => { video.srcObject = stream; }, (err) => console.error(err));
 }
 
+var blankTwo = []
+var blankThree = []
+
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
     document.body.append(canvas)
@@ -27,7 +30,43 @@ video.addEventListener('play', () => {
         
         var distanceBetweenLandmarks = Math.sqrt(Math.pow(rightEye[0]._x - leftEye[5]._x, 2) + Math.pow(rightEye[0]._y - leftEye[5]._y, 2))
         var roundedDistanceBetweenLandmarks = distanceBetweenLandmarks.toFixed(0);
-        document.getElementById("eyeLandmarkDifference").innerHTML = ("difference:", roundedDistanceBetweenLandmarks);
+        //document.getElementById("eyeLandmarkDifference").innerHTML = ("difference:", roundedDistanceBetweenLandmarks);
+        
+        rightEyeXPosition = rightEye[0]._x
+        rightEyeYPosition = rightEye[0]._y
+
+        var roundedRightEyeXPosition = [rightEyeXPosition.toFixed(0)];
+        var roundedRightEyeYPosition = [rightEyeYPosition.toFixed(0)];
+        blankTwo = blankTwo + ", " + roundedRightEyeXPosition;
+        blankThree = blankThree + ", " + roundedRightEyeYPosition;
+        
+        var splitRightEyeXPosition = blankTwo.split(", ");
+        var splitRightEyeYPosition = blankThree.split(", ");
+        var maxRightEyeXPosition = Math.max(...splitRightEyeXPosition);
+        var maxRightEyeYPosition = Math.max(...splitRightEyeYPosition);
+        
+        //console.log("right eye x-current", splitRightEyeXPosition[splitRightEyeXPosition.length - 1])
+        //console.log("right eye x-origin", splitRightEyeXPosition[1])
+        //console.log("right eye y-current", leftEye[5]._y)
+        //console.log("right eye y-current", splitRightEyeYPosition[splitRightEyeYPosition.length - 1])
+        //console.log("right eye y-origin", splitRightEyeYPosition[1])
+        //console.log("headDeviation", headDeviation)
+        
+        var headDeviation = Math.sqrt(Math.pow(splitRightEyeXPosition[splitRightEyeXPosition.length - 1] - splitRightEyeXPosition[1], 2) + Math.pow(splitRightEyeYPosition[splitRightEyeYPosition.length - 1] - splitRightEyeYPosition[1], 2))
+        var maxHeadDeviation = Math.sqrt(Math.pow(splitRightEyeXPosition[1] - maxRightEyeXPosition, 2) + Math.pow(splitRightEyeYPosition[1] - maxRightEyeYPosition, 2))
+        
+        var roundedHeadDeviation = [headDeviation.toFixed(0)];
+        var roundedMaxHeadDeviation = [maxHeadDeviation.toFixed(0)];
+        //console.log("splitRightEyeXPosition", splitRightEyeXPosition) 
+        //console.log("maxRightEyeXPosition[1]", maxRightEyeXPosition[1]) 
+
+        //console.log("splitRightEyeXPosition[1]", splitRightEyeXPosition[1])
+        //console.log("maxRightEyeXPosition", maxRightEyeXPosition)
+        
+        //maxHeadDeviation = Math.max(...splitHeadDeviation);
+        //var headDeviation = splitHeadDeviation[1] - maxHeadDeviation;
+        document.getElementById("roundedHeadDeviation").innerHTML = roundedHeadDeviation;        
+        document.getElementById("roundedMaxHeadDeviation").innerHTML = roundedMaxHeadDeviation;        
 
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
